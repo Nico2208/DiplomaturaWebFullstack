53d2ae7d48d41dc1d4e78c4e7f78e83d4b0aff4f
Nicolas Sicolo <sicolo2208@gmail.com>
2021-12-24 13:03:42 -0300

    modif js

--- M3/U4/Tarea/Ejercicio 1/js/funcionalidad.js
+++ M3/U4/Tarea/Ejercicio 1/js/funcionalidad.js
@@ -1,9 +1,40 @@
-var caja = document.getElementById('caja1');
+//Caja 1
 
+var caja = document.getElementById('caja1');
 
 caja.addEventListener('click', function(e){
     e.preventDefault();
-    alert('hola');
+    var contenido = document.getElementsByClassName('contenedor')[0];
+    if(contenido.style.backgroundColor != 'aquamarine')
+        contenido.style = 'background-color: aquamarine';
+    else
+    contenido.style = 'background-color: white';
+}, false);
+
+//Caja 2
+
+caja = document.getElementById('caja2');
+
+caja.addEventListener('click', function(e){
+    e.preventDefault();
+    var contenido = document.getElementsByClassName('contenedor')[0];
+    if(contenido.style.backgroundColor != 'salmon')
+        contenido.style = 'background-color: salmon';
+    else if (contenido.style.backgroundColor == 'salmon'){
+        contenido.style = 'background-color: white';}
+}, false);
+
+//Caja 3
+
+caja = document.getElementById('caja3');
+
+caja.addEventListener('click', function(e){
+    e.preventDefault();
+    var contenido = document.getElementsByClassName('contenedor')[0];
+    if(contenido.style.backgroundColor != 'burlywood')
+        contenido.style = 'background-color: burlywood';
+    else if(contenido.style.backgroundColor == 'burlywood'){
+        contenido.style = 'background-color: white';}
 }, false);
 
 
