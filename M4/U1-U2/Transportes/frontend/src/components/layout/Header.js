78a9cf43ac72743dd0e744056d816ce8fac9ab60
Nicolas Sicolo <sicolo2208@gmail.com>
2022-01-05 18:55:20 -0300

    nuevos cambios realizados

---
+++ M4/U1-U2/Transportes/frontend/src/components/layout/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = (props) => {
+    return (
+        <div>Header aguante el rockanroll</div>
+    );
+}
+
+export default Header;
---
+++ M4/U1-U2/Transportes/frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomePage = (props) => {
+    return (
+        <div>Esta es la home page</div>
+    );
+}
+
+export default HomePage;
+
