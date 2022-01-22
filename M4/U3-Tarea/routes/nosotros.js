d4aeb0ac8e521a0134c01b29c6c7999a867ee3a9
Nicolas Sicolo <sicolo2208@gmail.com>
2022-01-22 10:51:03 -0300

    cambios en las vias de acceso de rutas

--- M4/U3-Tarea/routes/nosotros.js
+++ M4/U3-Tarea/routes/nosotros.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET nosotros page. */
-router.get('/nosotros', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('nosotros');
 });
 
